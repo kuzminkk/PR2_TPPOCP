b7b6086bf8492dc0e44396306c33c9dbd5e0dde3
Kirill <kuzminkk5@gmail.com>
2025-10-04 19:55:31 +0300

    Загружен проект с локального репозитория

---
+++ tests/test_calculator.py
@@ -0,0 +1,19 @@
+from calculator import add, sub, mul, div
+import pytest
+
+def test_add():
+    assert add(2, 2) == 4   # проверка 2+2=4
+
+def test_sub():
+    assert sub(5, 3) == 2
+
+def test_mul():
+    assert mul(3, 4) == 12
+
+def test_div():
+    assert div(10, 2) == 5
+
+def test_div_zero():
+    import pytest
+    with pytest.raises(ValueError):
+        div(1, 0)
